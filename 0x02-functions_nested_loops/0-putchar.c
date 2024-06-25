b4485c1692fb02646bb039ef8629c30b748ce34c
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-06-25 12:06:47 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+/* declaring _putchar.c prototype */
+int _putchar(char);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
