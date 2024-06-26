d864e9062cab518c2d7f431aedb9109140658aee
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-06-26 00:15:52 +0000

    Fibonacci series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	unsigned long long fibo[50];
+
+	fibo[0] = 1;
+	fibo[1] = 2;
+
+	for (i = 0; i <= 49; i++)
+	{
+		fibo[i + 2] = fibo[i]  + fibo[i + 1];
+		printf("%llu, ", fibo[i]);
+	}
+	printf("\n");
+	return (0);
+}
