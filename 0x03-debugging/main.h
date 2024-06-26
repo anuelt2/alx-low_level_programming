9d6f5e710c45127df0cae24750d10739b46eb5da
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-06-26 18:22:24 +0000

    0 > 972?

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,4 +6,7 @@
 /* positive_or_negative function prototype */
 void positive_or_negative(int i);
 
+/* largest_number function prototype */
+int largest_number(int a, int b, int c);
+
 #endif /* MAIN_H */
