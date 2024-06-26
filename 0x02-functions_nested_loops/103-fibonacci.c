469d775a8f2de051372aa0d6c0068998003183f2
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-06-26 05:49:44 +0000

    fibonacci sum

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	long fib_1, fib_2, fibo, sum;
+
+	fib_1 = 1;
+	fib_2 = 2;
+
+	for (i = 0; i <= 49; i++)
+	{
+		if ((fib_2 % 2 == 0) && (fib_2 <= 4000000))
+		{
+			sum += fib_2;
+		}
+		fibo = fib_1 + fib_2;
+		fib_1 = fib_2;
+		fib_2 = fibo;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
