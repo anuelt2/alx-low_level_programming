6960bb77cadd3a978d35b22ab7d49852bbdc6df8
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-06-27 23:37:26 +0000

    Leap year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,4 +9,10 @@ void positive_or_negative(int i);
 /* largest_number function prototype */
 int largest_number(int a, int b, int c);
 
+/* convert_day function prototype */
+int convert_day(int month, int day);
+
+/* print_remaining_days function prototype */
+void print_remaining_days(int month, int day, int year);
+
 #endif /* MAIN_H */
