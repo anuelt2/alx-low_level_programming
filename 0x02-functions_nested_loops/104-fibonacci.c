ae3d290c88176ded88640d400103eaf4e2e33a98
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-06-27 00:48:17 +0000

    Fobinacci 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,25 +8,33 @@
 int main(void)
 {
 	int i;
-	unsigned long fib1;
-	unsigned long fib2;
-	unsigned long fib;
+	unsigned long fib1, fib2, fib, fib11, fib12, fib21, fib22;
 
 	fib1 = 1;
 	fib2 = 2;
 
-	printf("%lu, %lu, ", fib1, fib2);
-
-	for (i = 3; i <= 96; i++)
+	printf("%lu, %lu", fib1, fib2);
+	for (i = 3; i <= 91; i++)
 	{
 		fib = fib2;
 		fib2 = fib2 + fib1;
 		fib1 = fib;
-
-		if (i <= 96)
-			printf("%lu, ", fib2);
+		if (i <= 91)
+			printf(", %lu", fib2);
 	}
-	printf("%lu\n", fib2 + fib1);
-
+	fib11 = fib1 / 1000000000;
+	fib12 = fib1 % 1000000000;
+	fib21 = fib2 / 1000000000;
+	fib22 = fib2 % 1000000000;
+	for (i = 92; i <= 98; i++)
+	{
+		fib21 = fib21 + fib11;
+		fib11 = fib21 - fib11;
+		fib22 = fib22 + fib12;
+		fib12 = fib22 - fib12;
+		printf(", %lu", fib21 + (fib22 / 1000000000));
+		printf("%lu", fib22 % 1000000000);
+	}
+	printf("\n");
 	return (0);
 }
