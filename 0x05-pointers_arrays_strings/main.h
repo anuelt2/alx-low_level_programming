3abe49a7f117ea61abf82f00e3ee1253dfd98a35
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-02 03:23:29 +0000

    strcpy

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -31,4 +31,7 @@ void puts_half(char *str);
 /* print_array function prototype */
 void print_array(int *a, int n);
 
+/* _strcpy function prototype */
+char *_strcpy(char *dest, char *src);
+
 #endif
