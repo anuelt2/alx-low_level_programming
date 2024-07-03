a366a457acd20caac3cf1b1e1b63cf4e8d0d3190
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-03 21:33:58 +0000

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,19 +9,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i;
 
 	i = 0;
-	j = 0;
 
-	while (*(s1 + i) != '\0')
+	while (*(s1 + i) == *(s2 + i))
+	{
+		if (*(s1 + i) == '\0' || *(s2 + i) == '\0')
+			break;
 		i++;
-	while (*(s2 + j) != '\0')
-		j++;
-	if (i == j)
-		return (0);
-	else if (i < j)
-		return (-1);
-	else
-		return (1);
+	}
+
+	return (*(s1 + i) - *(s2 + i));
 }
