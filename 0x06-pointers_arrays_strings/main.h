13436f133073ce49b9afe2ffbf9370e1f0267555
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-03 16:03:48 +0000

    strncat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,4 +7,7 @@ int _putchar(char c);
 /* _strcat function prototype */
 char *_strcat(char *dest, char *src);
 
+/* _strncat functionn prototype */
+char *_strncat(char *dest, char *src, int n);
+
 #endif
