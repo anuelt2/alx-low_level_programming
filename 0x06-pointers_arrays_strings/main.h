eefc62a77e31389022e4bf0bbc6e544d76c2a11a
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-03 19:59:20 +0000

    strncpy

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,4 +10,7 @@ char *_strcat(char *dest, char *src);
 /* _strncat functionn prototype */
 char *_strncat(char *dest, char *src, int n);
 
+/* _strncpy function prototype */
+char *_strncpy(char *dest, char *src, int n);
+
 #endif
