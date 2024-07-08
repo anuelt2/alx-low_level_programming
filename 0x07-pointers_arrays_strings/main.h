1873de11ea9264b1fe04b4a4b238759ca00e2c81
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-08 16:06:34 +0000

    memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* _putchar function prototype */
+int _putchar(char c);
+
+/* _memset function prototype */
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
