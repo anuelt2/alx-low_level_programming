0a7c33368fe5ee0d1c6b7bc025bd37b1f43d8c77
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-08 23:36:17 +0000

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,11 +14,15 @@ char *_strchr(char *s, char c)
 
 	i = 0;
 
-	while (s[i] != '\0')
+	while (*(s + i) != '\0')
 	{
-		if (s[i] == c)
+		if (*(s + i) == c)
 			return (s + i);
 		i++;
 	}
+
+	if (c == '\0')
+		return (s + i);
+
 	return (NULL);
 }
