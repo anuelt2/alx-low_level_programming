9c26a8660bad87cba2bc909f7d700819224d0494
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-08 22:16:23 +0000

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,10 +12,12 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 
+	i = 0;
+
 	if (*needle == 0)
 		return (haystack);
 
-	for (i = 0; *haystack; i++)
+	while (*haystack)
 	{
 		if (*(haystack + i) == *(needle + i))
 		{
