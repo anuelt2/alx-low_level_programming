cfd4f43a9db7e55f5af1326d3f12228367acb6b8
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-10 10:53:14 +0000

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Check the code
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+
+	return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,4 +10,7 @@ void _puts_recursion(char *s);
 /* _print_rev_recursion function prototype */
 void _print_rev_recursion(char *s);
 
+/* _strlen_recursion function prototype */
+int _strlen_recursion(char *s);
+
 #endif
