38af352ee453848de717c0850778346922380bbe
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-12 18:55:28 +0000

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: First parameter
+ * @argv: Second parameter
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+
+	sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
