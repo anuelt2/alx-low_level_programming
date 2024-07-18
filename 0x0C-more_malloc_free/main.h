37ae5353ab91cb4b6d3110e1acf1157619ae4ab8
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-18 11:34:26 +0000

    Trust no one

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* _putchar function prototype */
+int _putchar(char c);
+
+/* malloc_checked function prototype */
+void *malloc_checked(unsigned int b);
+
+#endif
