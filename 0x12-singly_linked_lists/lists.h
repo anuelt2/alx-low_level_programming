62e68e0f166f70584302345c78ddfba77c4582c6
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-08-01 22:05:38 +0000

    Add add_node_end function prototype

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -33,4 +33,7 @@ size_t list_len(const list_t *h);
 /* add_node function prototype */
 list_t *add_node(list_t **head, const char *str);
 
+/* add_node_end function prototype */
+list_t *add_node_end(list_t **head, const char *str);
+
 #endif
