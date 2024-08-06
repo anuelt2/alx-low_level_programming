71753240b14f0c1789d062f0afb0e701a1a47d70
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-08-06 17:42:33 +0000

    Add reverse_listint function prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -55,4 +55,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 /* delete_nodeint_at_index function prototype */
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
+/* reverse_listint function prototype */
+listint_t *reverse_listint(listint_t **head);
+
 #endif
