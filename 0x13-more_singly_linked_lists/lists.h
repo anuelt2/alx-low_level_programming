e580b0f77b8218367601ed50766a5d9d65003e1c
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-08-06 13:47:34 +0000

    Add insert_nodeint_at_index function prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -49,4 +49,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 /* sum_listint function prototype */
 int sum_listint(listint_t *head);
 
+/* insert_nodeint_at_index function prototype */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+
 #endif
