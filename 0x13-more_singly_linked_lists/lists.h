c6583042df9229584e1ccfdceef21777aa461f67
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-08-06 14:32:05 +0000

    Add delete_nodeint_at_index function protototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -52,4 +52,7 @@ int sum_listint(listint_t *head);
 /* insert_nodeint_at_index function prototype */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 
+/* delete_nodeint_at_index function prototype */
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
 #endif
