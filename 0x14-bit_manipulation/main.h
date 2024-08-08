460f5e7d25a178307b933933ead47fc1181d7d01
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-08-08 15:14:45 +0000

    Add header file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+
+/* _putchar function prototype */
+int _putchar(char c);
+
+/* binary_to_uint function prototype */
+unsigned int binary_to_uint(const char *b);
+
+#endif
