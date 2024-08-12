7a1bf180ef071c34c1b744496da414a36dfba388
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-08-12 14:28:30 +0000

    Add header file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+/* _putchar function prototype */
+int _putchar(char c);
+
+/* read_textfile function prototype */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
