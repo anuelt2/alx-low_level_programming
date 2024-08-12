18f58450e33eef13af9206d8893e71e878dd1c7e
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-08-12 23:51:00 +0000

    Handling check errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,14 +8,15 @@ void process_errors(int src, int dest, char **av);
 /**
  * main - Entry point
  * @argc: Number of arguments passed
- * @argv: String of arguments passed
+ * @argv: Array of argument strings passed
  *
  * Return: Always 0 (Success)
  */
 
 int main(int argc, char **argv)
 {
-	int f_src, f_dest, r_content, w_content, cls, cld;
+	int f_src, f_dest, cls, cld;
+	ssize_t r_content, w_content;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -30,56 +31,62 @@ int main(int argc, char **argv)
 
 	f_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (f_dest == -1)
+	{
+		close(f_src);
 		process_errors(0, f_dest, argv);
+	}
 
 	r_content = 1024;
-	while (r_content == 1024)
+	while ((r_content = read(f_src, buffer, sizeof(buffer))) > 0)
 	{
-		r_content = read(f_src, buffer, sizeof(buffer));
-		if (r_content == -1)
-			process_errors(-1, 0, argv);
 		w_content = write(f_dest, buffer, r_content);
-		if (w_content == -1)
+		if (w_content == -1 || w_content != r_content)
+		{
+			close(f_src);
+			close(f_dest);
 			process_errors(0, -1, argv);
+		}
 	}
+	if (r_content == -1)
+		process_errors(-1, 0, argv);
 	cls = close(f_src);
 	if (cls == -1)
-		process_errors(-1, 0, 0);
+		process_errors(-1, 0, argv);
 
 	cld = close(f_dest);
 	if (cld == -1)
-		process_errors(0, -1, 0);
+		process_errors(0, -1, argv);
 
 	return (0);
 }
 
 /**
  * process_errors - Handles file processing errors
- * @src: Source file
- * @dest: Destination file
- * @av: Pointer to string arguments
+ * @src: Source file descriptor
+ * @dest: Destination file descriptor
+ * @av: Array of argument strings
  *
  * Return: Void
  */
 
 void process_errors(int src, int dest, char **av)
 {
-	if (src == -1)
+	if (src == -1 && av != NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	else if (dest == -1)
+	else if (dest == -1 && av != NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
 		exit(99);
 	}
-	else if (src == -1 && av == 0)
+	else if (src == -1 && av == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src);
 		exit(100);
 	}
-	else if (dest == -1 && av == 0)
+	else if (dest == -1 && av == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest);
 		exit(100);
