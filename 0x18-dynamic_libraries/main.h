234a3761205cf3b21d82e73973fb7f8d13c5c85c
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-09-24 10:14:42 +0000

    Add header file

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,64 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* _putchar function prototype */
+int _putchar(char c);
+
+/* _islower function prototype */
+int _islower(int c);
+
+/* _isalpha function prototype */
+int _isalpha(int c);
+
+/* _abs function prototype */
+int _abs(int n);
+
+/* _isupper function prototype */
+int _isupper(int c);
+
+/* _isdigit function prototype */
+int _isdigit(int c);
+
+/* _strlen function prototype */
+int _strlen(char *s);
+
+/* _puts function prototype */
+void _puts(char *s);
+
+/* _strcpy function prototype */
+char *_strcpy(char *dest, char *src);
+
+/* _atoi function prototype */
+int _atoi(char *s);
+
+/* _strcat function prototype */
+char *_strcat(char *dest, char *src);
+
+/* _strncat function prototype */
+char *_strncat(char *dest, char *src, int n);
+
+/* _strncpy function prototype */
+char *_strncpy(char *dest, char *src, int n);
+
+/* _strcmp function prototype */
+int _strcmp(char *s1, char *s2);
+
+/* _memset function prototype */
+char *_memset(char *s, char b, unsigned int n);
+
+/* _memcpy function prototype */
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+/* _strchr function prototype */
+char *_strchr(char *s, char c);
+
+/* _strspn function prototype */
+unsigned int _strspn(char *s, char *accept);
+
+/* _strpbrk function prototype */
+char *_strpbrk(char *s, char *accept);
+
+/* _strstr function prototype */
+char *_strstr(char *haystack, char *needle);
+
+#endif
