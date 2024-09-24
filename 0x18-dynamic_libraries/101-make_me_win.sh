4d9b0b368023a69d7239048e2555edab7be166d2
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-09-24 20:42:13 +0000

    Modify 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/anuelt2/alx-low_level_programming/blob/main/0x18-dynamic_libraries/tmp/myprintf.so
+wget -P /tmp https://github.com/anuelt2/alx-low_level_programming/raw/main/0x18-dynamic_libraries/myprintf.so
 export LD_PRELOAD=/tmp/myprintf.so:$LD_LIBRARY_PATH
