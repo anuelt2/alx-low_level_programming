e35129a5fbe58fbb507b6f9dd3c1747661c08494
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-10-11 09:44:28 +0000

    Add 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: Size of array
+ *
+ * Return: Pointer to newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	hash_table = calloc(1, sizeof(hash_table_t));
+	if (hash_table == NULL)
+	{
+		return (NULL);
+	}
+
+	hash_table->size = size;
+	hash_table->array = calloc(size, sizeof(hash_node_t));
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+
+	return (hash_table);
+}
