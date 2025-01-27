93df919f90463ca6b48bd0bf6aad7db18abe90ec
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-01-27 18:30:30 +0000

    Add jump_search function prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,11 +3,15 @@
 
 #include <stdio.h>
 #include <stddef.h>
+#include <math.h>
 
 /* linear_search function prototype */
 int linear_search(int *array, size_t size, int value);
 
 /* binary_search function prototype */
 int binary_search(int *array, size_t size, int value);
+
+/* jump_search function prototype */
+int jump_search(int *array, size_t size, int value);
 
 #endif
