8579d47c99bae722266119c13ebf42d829df4255
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-01-28 20:46:42 +0000

    Add linear_skip function prototype to search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -21,6 +21,24 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: Singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
 /* linear_search function prototype */
 int linear_search(int *array, size_t size, int value);
 
@@ -41,5 +59,8 @@ int advanced_binary(int *array, size_t size, int value);
 
 /* jump_list function prototype */
 listint_t *jump_list(listint_t *list, size_t size, int value);
+
+/* linear_skip function prototype */
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
