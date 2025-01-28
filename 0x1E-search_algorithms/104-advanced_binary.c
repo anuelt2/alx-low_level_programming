a6aa2bc6282bb766c69dfe3a13b249ddbcd514bd
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-01-28 11:46:40 +0000

    Add 104-advanced_binary.c

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,76 @@
+#include "search_algos.h"
+
+/**
+ * adv_bin_helper - Searches for the first occurrence of a value in a sorted
+ * array of integers using an advanced Binary search algorithm with recursion
+ * @array: Pointer to the first element in the array to search in
+ * @size: Number of elements in @array
+ * @value: The value to search for
+ * @min: Lower index of subarray to search
+ * @max: Upper index of subarray to search
+ *
+ * Return: Index where @value is located, -1 otherwise
+ */
+
+int adv_bin_helper(int *array, size_t size, int value, size_t min, size_t max)
+{
+	size_t i, mid;
+
+	if (array == NULL || size == 0 || min > max)
+		return (-1);
+
+	mid = min + (max - min) / 2;
+	printf("Searching in array: ");
+	for (i = min; i <= max; i++)
+	{
+		if (i < max)
+			printf("%d, ", array[i]);
+		else
+			printf("%d\n", array[i]);
+	}
+
+	if (value == array[mid])
+	{
+		if (mid == 0 || array[mid - 1] < value)
+		{
+			return (mid);
+		}
+		else
+		{
+			max = mid - 1;
+			return (adv_bin_helper(array, size, value, min, max));
+		}
+	}
+	if (value < array[mid])
+	{
+		max = mid - 1;
+		return (adv_bin_helper(array, size, value, min, max));
+	}
+	else
+	{
+		min = mid + 1;
+		return (adv_bin_helper(array, size, value, min, max));
+	}
+
+	return (-1);
+}
+
+/**
+ * advanced_binary - Searches for the first occurrence of a value in a sorted
+ * array of integers using an advanced Binary search algorithm with recursion
+ * @array: Pointer to the first element in the array to search in
+ * @size: Number of elements in @array
+ * @value: The value to search for
+ *
+ * Return: Index where @value is located, -1 otherwise
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t min, max;
+
+	min = 0;
+	max = size - 1;
+
+	return (adv_bin_helper(array, size, value, min, max));
+}
